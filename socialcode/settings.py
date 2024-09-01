6bdf259c7c6d0e14a972011f55cb997ab0786ac5
Pallav Rai <pallavrai8953@gmail.com>
2024-09-01 23:07:25 +0530

    Django cors  header added and tested in profile page

--- socialcode/settings.py
+++ socialcode/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = [
 	 "localhost"
 ]
 
-
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
 # Application definition
 
 INSTALLED_APPS = [
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'main'
 ]
 
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'socialcode.urls'
--- ui/src/routes/profile/[userid]/+page.svelte
+++ ui/src/routes/profile/[userid]/+page.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
 	export let data: {userid: string}
+	import { onMount } from 'svelte';
+
+	
+
+	onMount(async () => {
+	let data: any = null;
+	const response = await fetch('http://127.0.0.1:8000/api/profiles');
+	data = await response.json();
+	console.log(data);
+	});
 </script>
 
 <h3>
