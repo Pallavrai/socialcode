1dd10cb9de48df87b3e277819783239e65cb9439
Pallav Rai <pallavrai8953@gmail.com>
2024-09-01 17:50:22 +0530

    Changed with new navbar

--- ui/src/components/Header.svelte
+++ ui/src/components/Header.svelte
@@ -6,13 +6,20 @@
   </script>
   
  
-  <BottomNav {activeUrl} position="absolute" classInner="grid-cols-4">
+  <BottomNav {activeUrl} position="absolute" classInner="grid-cols-5">
     <BottomNavItem btnName="Home" href="/">
       <HomeSolid />
     </BottomNavItem>
     <BottomNavItem btnName="Chat" href="/docs/pages/quickstart" exact={false} >
       <MessageDotsSolid />
     </BottomNavItem>
+
+    <div class="flex items-center justify-center">
+    <BottomNavItem  appBtnPosition="middle" btnClass="inline-flex items-center justify-center w-10 h-10 font-medium bg-primary-600 rounded-full hover:bg-primary-700 group focus:ring-4 focus:ring-primary-300 focus:outline-none dark:focus:ring-primary-800">
+      <PlusOutline class="text-white" />
+    </BottomNavItem>
+  </div>
+    
     <BottomNavItem btnName="Settings" href="/docs/components/bottom-navigation">
       <AdjustmentsVerticalOutline />
     </BottomNavItem>
